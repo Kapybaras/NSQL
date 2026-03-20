08819c9b06b3b31bde5a297c3dc9a2afd09d93f5
MaxIsAQT1 <sophie.sharpe12@gmail.com>
2026-03-20 05:11:29 +0000

    Add application-secrets to classpath for spring @Autowired

--- nsql/src/main/java/uk/ac/bangor/cs/dyp24nbv/AisqlApplication.java
+++ nsql/src/main/java/uk/ac/bangor/cs/dyp24nbv/AisqlApplication.java
@@ -2,8 +2,10 @@ package uk.ac.bangor.cs.dyp24nbv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-secrets.properties")
 public class AisqlApplication {
 
 	public static void main(String[] args) {
